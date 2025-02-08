ab7cbc1b6b2c28ae009845fea75d3b4448a02fe9
kinghorey <oreoluwaakinbo.oa@gmail.com>
2025-02-08 19:51:52 +0100

    set token in cookie

--- frontend/app/utilities/utils.ts
+++ frontend/app/utilities/utils.ts
@@ -1,6 +1,9 @@
 "use server";
 
 import { cookies } from "next/headers";
+import { jwtDecode } from "jwt-decode";
+
+import { JWTPayload } from "@/lib/definitions";
 
 export const handleLoginToken = async (x) => {
   const cookieStore = await cookies();
@@ -10,4 +13,32 @@ export const handleLoginToken = async (x) => {
   const refreshToken = process.env.NEXT_REFRESH_TOKEN || "refresh_token";
   cookieStore.set(accessToken, access);
   cookieStore.set(refreshToken, refresh);
+
+  const decodedToken: JWTPayload = jwtDecode(access);
+  const { full_name, email, user_id, language, currency, timezone } =
+    decodedToken;
+
+  const user = {
+    full_name,
+    email,
+    user_id,
+  };
+
+  cookieStore.set("_user_attributes", JSON.stringify(user), {
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "strict",
+    maxAge: 60 * 60 * 24 * 7,
+  });
+
+  cookieStore.set("language", language, {
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "strict",
+    maxAge: 60 * 60 * 24 * 7,
+  });
+
+  cookieStore.set("_user_attributes", currency, {
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "strict",
+    maxAge: 60 * 60 * 24 * 7,
+  });
 };
--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -1,6 +1,5 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
-
 import { jwtDecode } from "jwt-decode";
 import { handleTokenRefresh } from "./app/utilities/handleTokenRefresh";
 
